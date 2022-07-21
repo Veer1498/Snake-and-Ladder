45fb2b5eceacb47d1ddb82abddd52fdb1636a8b6
Veer1498 <singavarapuveerendra@gmail.com>
2022-07-22 01:25:57 +0530

    [Veer] Added : Single Player Starting Game at 0

--- src/corejava/SnakeLadder.java
+++ src/corejava/SnakeLadder.java
@@ -5,7 +5,8 @@ public class SnakeLadder {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Snake Ladder Program");
-
+		int playerPosition = 0;
+		System.out.println("Position of the player Before Game start : "+playerPosition);
 	}
 
 }
