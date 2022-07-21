b6c1d5d34ed185c1bfeff1614ded962cb1853698
Veer1498 <singavarapuveerendra@gmail.com>
2022-07-22 01:12:31 +0530

    [Veer] Added : Welcome Message

---
+++ src/corejava/SnakeLadder.java
@@ -0,0 +1,11 @@
+package corejava;
+
+public class SnakeLadder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Snake Ladder Program");
+
+	}
+
+}
